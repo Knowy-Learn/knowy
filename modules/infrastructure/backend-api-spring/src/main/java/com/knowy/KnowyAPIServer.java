600c7be9a9a6477e563ec0c67735bb94c2bea9a0
Javier Gavilán Mérida <jagame90@gmail.com>
2025-08-18 21:51:48 +0200

    Added backend-api-spring module (currently empty, only has basic dependencies)

---
+++ modules/infrastructure/backend-api-spring/src/main/java/com/knowy/KnowyAPIServer.java
@@ -0,0 +1,13 @@
+package com.knowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowyAPIServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowyAPIServer.class, args);
+	}
+
+}
