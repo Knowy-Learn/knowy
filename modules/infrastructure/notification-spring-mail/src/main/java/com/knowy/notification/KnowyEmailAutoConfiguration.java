01e082eb121df57c9f548fd70fdb1ea920802ed3
Javier Gavilán Mérida <jagame90@gmail.com>
2025-08-22 17:18:29 +0200

    Fixed notification-spring-mail autoconfiguration

--- modules/infrastructure/notification-spring-mail/src/main/java/com/knowy/notification/KnowyEmailAutoConfiguration.java
+++ modules/infrastructure/notification-spring-mail/src/main/java/com/knowy/notification/KnowyEmailAutoConfiguration.java
@@ -1,10 +1,17 @@
 package com.knowy.notification;
 
+import com.knowy.core.port.KnowyNotificationDispatcher;
+import com.knowy.notification.adapter.mail.EmailDispatcher;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @AutoConfiguration
-@ComponentScan
 public class KnowyEmailAutoConfiguration {
+
+	@Bean
+	public KnowyNotificationDispatcher emailDispatcher(JavaMailSender javaMailSender) {
+		return new EmailDispatcher(javaMailSender);
+	}
 
 }
