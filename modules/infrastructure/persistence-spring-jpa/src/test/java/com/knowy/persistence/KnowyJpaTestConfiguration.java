6c707fcf0f14b5179ca95b36d4dc6861012ef6c1
saramforte <saraforte310@gmail.com>
2025-10-21 09:23:35 +0200

    Removed unnecessary @EnableAutoConfiguration

--- modules/infrastructure/persistence-spring-jpa/src/test/java/com/knowy/persistence/KnowyJpaTestConfiguration.java
+++ modules/infrastructure/persistence-spring-jpa/src/test/java/com/knowy/persistence/KnowyJpaTestConfiguration.java
@@ -1,9 +1,7 @@
 package com.knowy.persistence;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Import;
 
-@EnableAutoConfiguration
 @Import({KnowyJpaMapperAutoConfiguration.class, KnowyJpaRepositoryAutoConfiguration.class})
 public class KnowyJpaTestConfiguration {
 
