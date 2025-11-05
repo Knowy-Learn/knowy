177c8b0033aaced8409dd382b9ae06e1532e7a16
saramforte <saraforte310@gmail.com>
2025-11-05 17:28:10 +0100

    feature/#114 - Set up Astro and built the landing page layout
    patch: configurated astro with tailwind and prettier
    minor: set up the base of landing page in file index.astro

---
+++ modules/infrastructure/frontend-web-astro/knowy-web-astro/prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+export default {
+	plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+	useTabs: true,
+	singleQuote: false,
+	trailingComma: 'none',
+	semi: false,
+	printWidth: 120,
+	pluginSearchDirs: false
+}
