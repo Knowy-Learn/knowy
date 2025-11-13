e3c53677824634e96e18ead7817d7c9599841f84
saramforte <saraforte310@gmail.com>
2025-11-13 12:47:51 +0100

    feature/#114 - Restructuring and standardization of frontend and backend modules
    patch: Renamed `frontend-web-spring` to `frontend-spring-web-thymeleaf`
    patch: Renamed `frontend-web-astro` to `web-astro`
    patch: Moved `KnowyAPIServer.java` to `com.knowy.server`
    minor: Created new module `frontend-spring-web-astro` (Spring + Astro)
    patch: Added new `application.yaml` configuration files to corresponding modules
    patch: Updated root `pom.xml` to reflect new module structure

---
+++ modules/infrastructure/frontend-spring-web-astro/src/main/java/com/knowy/frontend/KnowyServerApplication.java
@@ -0,0 +1,13 @@
+package com.knowy.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowyServerApplication.class, args);
+	}
+}
+
--- modules/infrastructure/web-astro/knowy-web-astro/astro.config.mjs
+++ modules/infrastructure/web-astro/knowy-web-astro/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config"
 import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
-	outDir: '../target/dist',
+	outDir: '../target/classes/public',
 	vite: {
 		plugins: [tailwindcss()],
 	}
