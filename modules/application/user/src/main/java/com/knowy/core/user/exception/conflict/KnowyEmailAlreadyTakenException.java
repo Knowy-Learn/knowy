4c13ba6e94fd0281aed65131575ed74ccd9a899b
saramforte <saraforte310@gmail.com>
2025-12-22 13:42:15 +0100

    feature/#115 - patch: Changed heritage of KnowyEmailAlreadyTakenException

--- modules/application/user/src/main/java/com/knowy/core/user/exception/conflict/KnowyEmailAlreadyTakenException.java
+++ modules/application/user/src/main/java/com/knowy/core/user/exception/conflict/KnowyEmailAlreadyTakenException.java
@@ -1,8 +1,8 @@
 package com.knowy.core.user.exception.conflict;
 
-import com.knowy.core.exception.KnowyException;
+import com.knowy.core.exception.validation.KnowyConflictException;
 
-public class KnowyEmailAlreadyTakenException extends KnowyException {
+public class KnowyEmailAlreadyTakenException extends KnowyConflictException {
 	public KnowyEmailAlreadyTakenException(String message) {
 		super(message);
 	}
