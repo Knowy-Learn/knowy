283ddab873fc1e64d5c6b728545a627ec335d628
saramforte <saraforte310@gmail.com>
2026-02-04 10:55:50 +0100

    feature/#115 - Added exception error to UserResumeGet

--- modules/infrastructure/backend-api-spring/src/main/java/com/knowy/server/api/controller/UserController.java
+++ modules/infrastructure/backend-api-spring/src/main/java/com/knowy/server/api/controller/UserController.java
@@ -5,6 +5,7 @@ import com.knowy.core.port.CourseRepository;
 import com.knowy.core.port.LessonRepository;
 import com.knowy.core.port.UserCourseRepository;
 import com.knowy.core.port.UserLessonRepository;
+import com.knowy.server.api.controller.exception.KnowyInternalServerErrorException;
 import com.knowy.server.api.dto.*;
 import com.knowy.server.api.usecase.user.GetLearnCoursesDataUseCase;
 import com.knowy.server.api.usecase.user.GetNavbarUserDataUseCase;
@@ -100,7 +101,7 @@ public class UserController implements UserApi {
 		try {
 			return ResponseEntity.ok(getResumeUserDataUseCase.execute());
 		} catch (KnowyInconsistentDataException e) {
-			throw new RuntimeException(e); // TODO
+			throw new KnowyInternalServerErrorException("Data inconsistency detected while fetching user resume", e);
 		}
 	}
 }
