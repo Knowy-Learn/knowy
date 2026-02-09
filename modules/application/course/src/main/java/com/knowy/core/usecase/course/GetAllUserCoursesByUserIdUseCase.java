96e33ce525503cb2ea6cc9bb132bbedcfbecf046
saramforte <saraforte310@gmail.com>
2026-02-09 11:07:53 +0100

    feature/#115 - Implemented category filter validations

--- modules/application/course/src/main/java/com/knowy/core/usecase/course/GetAllUserCoursesByUserIdUseCase.java
+++ modules/application/course/src/main/java/com/knowy/core/usecase/course/GetAllUserCoursesByUserIdUseCase.java
@@ -1,12 +1,11 @@
 package com.knowy.core.usecase.course;
 
-import com.knowy.core.domain.CourseStatus;
-import com.knowy.core.domain.PagedResult;
-import com.knowy.core.domain.Pagination;
-import com.knowy.core.domain.UserCourse;
+import com.knowy.core.domain.*;
 import com.knowy.core.exception.data.KnowyDataAccessException;
+import com.knowy.core.exception.validation.KnowyIllegalArgumentRuntimeException;
 import com.knowy.core.port.UserCourseRepository;
 
+import java.util.Optional;
 import java.util.Set;
 
 /**
@@ -34,7 +33,33 @@ public class GetAllUserCoursesByUserIdUseCase {
 	 * @return a {@link PagedResult} containing a list of {@link UserCourse} objects and metadata
 	 */
 	public PagedResult<UserCourse> execute(int userId, Set<CourseStatus> courseStatusIds, Pagination pagination) throws KnowyDataAccessException {
+		checkCategoryFilterOperator(pagination);
 		return userCourseRepository.findAllByUserId(userId, ensureNotEmpty(courseStatusIds), pagination);
+	}
+
+	private void checkCategoryFilterOperator(Pagination pagination) {
+		findCategoryFilter(pagination)
+			.ifPresent(filter -> {
+				validateOperator(filter);
+				validateValue(filter);
+			});
+	}
+
+	private Optional<Filter> findCategoryFilter(Pagination pagination) {
+		return pagination.filters().stream()
+			.filter(filter -> "category".equals(filter.value()))
+			.findFirst();
+	}
+
+	private void validateOperator(Filter filter) {
+		if (filter.operator() != Filter.Operator.IN) {
+			throw new KnowyIllegalArgumentRuntimeException("Category filter must use 'IN' operator.");
+		}
+	}
+	private void validateValue(Filter filter) {
+		if (filter.value() instanceof Set<?>) {
+			throw new KnowyIllegalArgumentRuntimeException("Category filter must be Set collection");
+		}
 	}
 
 	private Set<CourseStatus> ensureNotEmpty(Set<CourseStatus> courseStatuses) {
